057255f566840df4a7590dda82e1eba49fc075d9
kalanithith <34536605+kalanithith@users.noreply.github.com>
2017-12-14 19:50:29 +0530

    Create maxnum.cpp

---
+++ maxnum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+int a,b,c;
+cin>>a>>b>>c;
+if(a>b)
+cout<<"\nmax num:"<<a;
+else if(b>c)
+cout<<"\nmax num:"<<b;
+else
+cout<<"\nmax num:"<<c;
+return 0;
+}
