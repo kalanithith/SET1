0f6f76e149dc7c5dcec27c52e91579cf1a2f0d6e
kalanithith <34536605+kalanithith@users.noreply.github.com>
2017-12-26 19:13:56 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Square
+{
+	public static void main (String[] args)
+	{
+	  int a,n;
+	  Scanner ob=new Scanner(System.in);
+	  a=ob.nextInt();
+	  n=ob.nextInt();
+    System.out.println("result:"+Math.pow(a,n));
+	}
+}
